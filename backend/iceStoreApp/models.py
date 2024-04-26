d1e0d5c325414d2922541ab6c824aa1f2a63eb49
CezaryPalczewski <cezarypalczewski@o2.pl>
2024-04-26 14:35:47 +0200

    fixed Items table

--- backend/iceStoreApp/models.py
+++ backend/iceStoreApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Items(models.Model):
-	item_id = models.IntegerField()
+	item_id = models.AutoField(primary_key=True)
 	item_name = models.CharField(max_length=50)
-	price = models.models.FloatField()
+	price = models.FloatField()
 	amount = models.IntegerField()
