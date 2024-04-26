efe1ae2103d964dec43c185ab72f317f5105bbc8
unknown <user@V03O.edu2.zsl.poznan.pl>
2024-04-26 14:33:34 +0200

    added fake items implementation

--- backend/iceStoreApp/views.py
+++ backend/iceStoreApp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import HttpResponse
+from django.core import serializers
+import json
 
-# Create your views here.
+class StoreItemsView(APIView):
+    def get(self, request):
+
+        sample_data = {
+            'items': {
+                'item1': {
+                    'item_id': '1',
+                    'item_name': 'name1',
+                    'price': '1',
+                    'amount': '1',
+                },
+                'item2': {
+                    'item_id': '2',
+                    'item_name': 'name2',
+                    'price': '2',
+                    'amount': '2',
+                },  
+            }
+        }
+        fake_items_implementation = json.dumps(sample_data, indent=4)
+        return HttpResponse(fake_items_implementation, 'application/json')
