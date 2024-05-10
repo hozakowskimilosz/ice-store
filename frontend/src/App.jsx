a291c40d59c80fbebc309a01fd83d62ab8e9f555
mhozakowski <mhozakowski@gmail.com>
2024-05-10 13:34:14 +0300

    implemented dark mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,15 @@
 import Products from "./components/Products";
 import Layout from "./components/Layout";
-import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/products" element={<Products />} />
-          </Route>
-        </Routes>
-      </Router>
-    </ChakraProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/products" element={<Products />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,27 @@
 import { FiCoffee } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { Button } from "@chakra-ui/react";
+import { useColorMode } from "@chakra-ui/react";
+import { FaSun } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
 
 export default function Header() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <div className="flex h-[8rem] items-end gap-3 rounded-md bg-blue-500 p-4 text-5xl font-semibold">
+    <div className="flex h-[8rem] items-end justify-between gap-3 rounded-md bg-blue-500 p-4 text-5xl font-semibold">
       <Link to="/" className="flex items-end gap-3">
         <h1 className="font-robotoCondensed font-bold tracking-tight">
           Ice Store
         </h1>
         <FiCoffee />
       </Link>
+
+      <div>
+        <Button onClick={toggleColorMode}>
+          {colorMode === "light" ? <FaMoon /> : <FaSun />}
+        </Button>
+      </div>
     </div>
   );
 }
