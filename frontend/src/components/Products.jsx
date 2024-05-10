821b0943d19f5fa045f0c84e8fcd996e7499b30c
albert <szamipl@gmail.com>
2024-05-10 13:27:59 +0200

    displayed products

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,3 +1,33 @@
+import React, { useState, useEffect } from "react";
+
 export default function Products() {
-  return <div>Products</div>;
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/items/")
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data.items);
+        setLoading(false);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Products:</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.item_id}>
+            {item.item_name} - {item.price} zł
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
